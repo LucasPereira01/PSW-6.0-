0b2f0e67dc4dcb27ee527c1ec4d142c29589372b
Lucas Carmo <lucaspdocarmo@gmail.com>
2023-05-08 23:43:05 -0300

    alteraçao do Procifle

--- type_event/settings.py
+++ type_event/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join('static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =os.path.join('staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
