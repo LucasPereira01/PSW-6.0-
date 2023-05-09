03a6e26bcf65207077b08367edeba25513875a09
Lucas Carmo <lucaspdocarmo@gmail.com>
2023-05-09 10:07:57 -0300

    alteração Static FIles

--- type_event/settings.py
+++ type_event/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = 'C:\PYTHON\myAplyLucarm\static'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
