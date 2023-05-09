8df003f5ca1a28780a496c3574771f1b7fb58e52
Lucas Carmo <lucaspdocarmo@gmail.com>
2023-05-08 23:34:50 -0300

    alteraçao do Procifle

--- type_event/settings.py
+++ type_event/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'cloudinary_storage',
-    'cloudinary',
     'usuarios',
     'eventos',
     'cliente',
@@ -82,11 +80,7 @@ WSGI_APPLICATION = "type_event.wsgi.application"
 
 
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': '',
-    'API_KEY': '',
-    'API_SECRET': ''
-}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -134,8 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+#STATIC_ROOT = os.path.join('static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
